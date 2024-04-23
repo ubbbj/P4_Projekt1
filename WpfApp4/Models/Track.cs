29e749790f658e4fd1552ddee9bacb8c9c2c131b
ubbbj <148093260+ubbbj@users.noreply.github.com>
2024-04-23 14:18:35 +0200

    .

--- WpfApp4/Models/Track.cs
+++
@@ -1,8 +0,0 @@
-namespace SpotifyRecommendationApp.Models;
-
-public class Track
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Artist { get; set; }
-}
